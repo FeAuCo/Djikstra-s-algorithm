4e7bdfbc1b291bc473e7bc610f1f3a2d695ddaa1
FeAuCo <vladikyudin09@gmail.com>
2024-11-16 22:31:22 +0300

    extendFrontier fixed

--- core/src/main/java/io/github/FeAuCo/algorithms/Djikstra.java
+++ core/src/main/java/io/github/FeAuCo/algorithms/Djikstra.java
@@ -7,24 +7,37 @@ import java.util.ArrayList;
 
 public class Djikstra {
     private static ArrayList<Node> frontier;
-    private static Node agent;
     private static ArrayList<Node> agents;
 
     private static void run(ArrayList<Node[]> nodes, Node startNode){
-        agent = startNode;
+        agents.add(startNode);
+
         boolean end = false;
 
-
         while (!end){
+            for (Node agent : agents){
+                if (agent.getNodeType().equals(NodeTypes.END)){
+                    end = true;
+                    break;
+                }
+
+                frontier.addAll(extendFrontier(nodes, frontier, agent));
 
 
+            }
+
+            if (frontier.isEmpty()){
+                // NO ROUTE
+                break;
+            }
         }
     }
 
-    private static void extendFrontier(ArrayList<Node[]> nodes, Node agent){
+    private static ArrayList<Node> extendFrontier(ArrayList<Node[]> nodes, ArrayList<Node> frontier,Node agent){
         if (agent.getCoordinates()[1] < 701){
             if (!nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.VISITED) ||
-                !nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.BARRIER)){
+                !nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.BARRIER) ||
+                !nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.START)){
 
                 nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0]].setPreviousNode(agent);
                 frontier.add(nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0]]);
@@ -33,7 +46,8 @@ public class Djikstra {
 
         if (agent.getCoordinates()[1] > 8){
             if (!nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.VISITED) ||
-                !nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.BARRIER)){
+                !nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.BARRIER) ||
+                !nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.START)){
 
                 nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0]].setPreviousNode(agent);
                 frontier.add(nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0]]);
@@ -42,7 +56,8 @@ public class Djikstra {
 
         if (agent.getCoordinates()[0] < 867){
             if (!nodes.get(agent.getIndices()[1])[agent.getIndices()[0] + 1].getNodeType().equals(NodeTypes.VISITED) ||
-                !nodes.get(agent.getIndices()[1])[agent.getIndices()[0] + 1].getNodeType().equals(NodeTypes.BARRIER)){
+                !nodes.get(agent.getIndices()[1])[agent.getIndices()[0] + 1].getNodeType().equals(NodeTypes.BARRIER) ||
+                !nodes.get(agent.getIndices()[1])[agent.getIndices()[0] + 1].getNodeType().equals(NodeTypes.START)){
 
                 nodes.get(agent.getIndices()[1])[agent.getIndices()[0] + 1].setPreviousNode(agent);
                 frontier.add(nodes.get(agent.getIndices()[1])[agent.getIndices()[0] + 1]);
@@ -51,11 +66,97 @@ public class Djikstra {
 
         if (agent.getCoordinates()[0] > 6){
             if (!nodes.get(agent.getIndices()[1])[agent.getIndices()[0] - 1].getNodeType().equals(NodeTypes.VISITED) ||
-                !nodes.get(agent.getIndices()[1])[agent.getIndices()[0] - 1].getNodeType().equals(NodeTypes.BARRIER)){
+                !nodes.get(agent.getIndices()[1])[agent.getIndices()[0] - 1].getNodeType().equals(NodeTypes.BARRIER) ||
+                !nodes.get(agent.getIndices()[1])[agent.getIndices()[0] - 1].getNodeType().equals(NodeTypes.START)){
 
                 nodes.get(agent.getIndices()[1])[agent.getIndices()[0] - 1].setPreviousNode(agent);
                 frontier.add(nodes.get(agent.getIndices()[1])[agent.getIndices()[0] - 1]);
             }
         }
+
+
+        if (agent.getCoordinates()[1] < 701 && agent.getCoordinates()[0] < 867){
+            if (!nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0] + 1].getNodeType().equals(NodeTypes.VISITED) ||
+                !nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0] + 1].getNodeType().equals(NodeTypes.BARRIER) ||
+                !nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0] + 1].getNodeType().equals(NodeTypes.START)){
+
+                if ((!nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.VISITED) ||
+                    !nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.BARRIER) ||
+                    !nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.START))
+                    &&
+                    (!nodes.get(agent.getIndices()[1])[agent.getIndices()[0] + 1].getNodeType().equals(NodeTypes.VISITED) ||
+                    !nodes.get(agent.getIndices()[1])[agent.getIndices()[0] + 1].getNodeType().equals(NodeTypes.BARRIER) ||
+                    !nodes.get(agent.getIndices()[1])[agent.getIndices()[0] + 1].getNodeType().equals(NodeTypes.START))){
+
+
+                    nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0] + 1].setPreviousNode(agent);
+                    frontier.add(nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0] + 1]);
+                }
+            }
+        }
+
+        if (agent.getCoordinates()[1] > 8 && agent.getCoordinates()[0] > 6){
+            if (!nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0] - 1].getNodeType().equals(NodeTypes.VISITED) ||
+                !nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0] - 1].getNodeType().equals(NodeTypes.BARRIER) ||
+                !nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0] - 1].getNodeType().equals(NodeTypes.START)){
+
+                if ((!nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.VISITED) ||
+                    !nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.BARRIER) ||
+                    !nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.START))
+                    &&
+                    (!nodes.get(agent.getIndices()[1])[agent.getIndices()[0] - 1].getNodeType().equals(NodeTypes.VISITED) ||
+                        !nodes.get(agent.getIndices()[1])[agent.getIndices()[0] - 1].getNodeType().equals(NodeTypes.BARRIER) ||
+                        !nodes.get(agent.getIndices()[1])[agent.getIndices()[0] - 1].getNodeType().equals(NodeTypes.START))){
+
+
+                    nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0] - 1].setPreviousNode(agent);
+                    frontier.add(nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0] - 1]);
+                }
+            }
+        }
+
+        if (agent.getCoordinates()[1] > 8 && agent.getCoordinates()[0] < 867){
+            if (!nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0] + 1].getNodeType().equals(NodeTypes.VISITED) ||
+                !nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0] + 1].getNodeType().equals(NodeTypes.BARRIER) ||
+                !nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0] + 1].getNodeType().equals(NodeTypes.START)){
+
+                if ((!nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.VISITED) ||
+                    !nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.BARRIER) ||
+                    !nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.START))
+                    &&
+                    (!nodes.get(agent.getIndices()[1])[agent.getIndices()[0] + 1].getNodeType().equals(NodeTypes.VISITED) ||
+                    !nodes.get(agent.getIndices()[1])[agent.getIndices()[0] + 1].getNodeType().equals(NodeTypes.BARRIER) ||
+                    !nodes.get(agent.getIndices()[1])[agent.getIndices()[0] + 1].getNodeType().equals(NodeTypes.START))){
+
+
+                    nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0] + 1].setPreviousNode(agent);
+                    frontier.add(nodes.get(agent.getIndices()[1] - 1)[agent.getIndices()[0] + 1]);
+                }
+            }
+        }
+
+        if (agent.getCoordinates()[1] < 701 && agent.getCoordinates()[0] > 6){
+            if (!nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0] - 1].getNodeType().equals(NodeTypes.VISITED) ||
+                !nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0] - 1].getNodeType().equals(NodeTypes.BARRIER) ||
+                !nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0] - 1].getNodeType().equals(NodeTypes.START)){
+
+                if ((!nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.VISITED) ||
+                    !nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.BARRIER) ||
+                    !nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0]].getNodeType().equals(NodeTypes.START))
+                    &&
+                    (!nodes.get(agent.getIndices()[1])[agent.getIndices()[0] - 1].getNodeType().equals(NodeTypes.VISITED) ||
+                        !nodes.get(agent.getIndices()[1])[agent.getIndices()[0] - 1].getNodeType().equals(NodeTypes.BARRIER) ||
+                        !nodes.get(agent.getIndices()[1])[agent.getIndices()[0] - 1].getNodeType().equals(NodeTypes.START))){
+
+
+                    nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0] - 1].setPreviousNode(agent);
+                    frontier.add(nodes.get(agent.getIndices()[1] + 1)[agent.getIndices()[0] - 1]);
+                }
+            }
+        }
+
+
+
+        return frontier;
     }
 }
